aa8739d0ae4d8c97b0c4fe784b1089801f9a2a26
voyager418 <george.kg@outlook.com>
2024-01-20 21:35:54 +0100

    Initial commit

---
+++ src/main/java/historicaldatafetcher/HistoricalDataHandler.java
@@ -0,0 +1,128 @@
+package src.main.java.historicaldatafetcher;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
+import com.ib.client.Contract;
+import com.ib.client.Types;
+import com.ib.controller.ApiController;
+import com.opencsv.CSVWriter;
+
+public class HistoricalDataHandler implements ApiController.IHistoricalDataHandler {
+	private final CSVWriter writer;
+	private static final SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd");
+	private String barTimestamp; // will always hold the most recent timestamp
+	private boolean shouldFetchRemainingData;
+	private final ApiController apiController;
+	private final Types.BarSize barSize;
+	private final Types.WhatToShow whatToShow;
+	private Calendar from;
+	private Calendar endDate;
+	private Calendar to;
+	private final Contract contract;
+
+	public HistoricalDataHandler(ApiController apiController, Types.BarSize barSize, Types.WhatToShow whatToShow,
+			Calendar from, Calendar to, Contract contract, boolean shouldFetchRemainingData) throws IOException {
+		this.apiController = apiController;
+		this.barSize = barSize;
+		this.whatToShow = whatToShow;
+		this.from = from;
+		this.endDate = to;
+		this.to = to;
+		this.contract = contract;
+		this.shouldFetchRemainingData = shouldFetchRemainingData;
+		String fileName = this.getFileName(contract, barSize);
+		File file = new File(fileName);
+		FileWriter outputFile = new FileWriter(file);
+		writer = new CSVWriter(outputFile);
+		String[] header = { "timestamp", "open", "high", "low", "close", "volume" };
+		writer.writeNext(header);
+	}
+
+	@Override
+	public void historicalDataEnd() {
+		if (!this.shouldFetchRemainingData ||
+				getDifferenceOfDays(new Date(Timestamp.from(Instant.parse(barTimestamp)).getTime()), this.to.getTime()) <= 5) {
+			try {
+				writer.close();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+			System.exit(0);
+		}
+
+		Timestamp lastFetchedTimestamp = Timestamp.from(Instant.parse(barTimestamp));
+		int duration = getDifferenceOfDays(new Date(lastFetchedTimestamp.getTime()), new Date(System.currentTimeMillis()));
+		if (duration > 356) {
+			// year = year + 1
+			endDate.set(endDate.getTime().getYear() + 1901, Calendar.JANUARY, 1, 0, 0, 0);
+		} else {
+			// current year
+			this.shouldFetchRemainingData = false;
+			endDate = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+		}
+		apiController.reqHistoricalData(this.contract,
+				DATE_TIME_FORMAT.format(endDate.getTime()) + " UTC",
+				duration > 365 ? 1 : duration,
+				duration > 365 ? Types.DurationUnit.YEAR : Types.DurationUnit.DAY,
+				this.barSize,
+				whatToShow,
+				true, false, this);
+	}
+
+	public void fetchCandlesticks() {
+		int duration = getDifferenceOfDays(from.getTime(), to.getTime());
+		duration = duration >= 365 ? 1 : duration;
+		Types.DurationUnit durationUnit = Types.DurationUnit.DAY;
+		if (duration == 1) {
+			durationUnit = Types.DurationUnit.YEAR;
+		}
+
+		this.apiController.reqHistoricalData(contract,
+				DATE_TIME_FORMAT.format(to.getTime()) + " UTC",
+				duration,
+				durationUnit,
+				barSize,
+				whatToShow,
+				true, false, this);
+	}
+
+	public static int getDifferenceOfDays(Date d1, Date d2) {
+		long diff = d2.getTime() - d1.getTime();
+		return (int) TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+
+	private String getFileName(Contract contract, Types.BarSize barSize) {
+		return contract.symbol() + "-" + contract.currency() + "-" +
+				contract.exchange() + "-" + barSize.toString().replace(" ", "") + ".csv";
+	}
+
+	@Override
+	public void historicalData(com.ib.client.Bar bar) {
+		try {
+			Instant barInstant = DATE_TIME_FORMAT.parse(bar.time()).toInstant();
+			barInstant = barInstant.plusSeconds(7200); // 2 hours to covert from UTC to New York time
+			barTimestamp = barInstant.toString();
+		} catch (ParseException e) {
+			try {
+				barTimestamp = DATE_FORMAT.parse(bar.time()).toInstant().toString();
+			} catch (ParseException e2) {
+				throw new RuntimeException(e2);
+			}
+		}
+
+		String[] data = { barTimestamp, String.valueOf(bar.open()), String.valueOf(bar.high()), String.valueOf(bar.low()),
+				String.valueOf(bar.close()), String.valueOf(bar.volume()) };
+		writer.writeNext(data);
+	}
+}
---
+++ src/main/java/historicaldatafetcher/Main.java
@@ -0,0 +1,81 @@
+package src.main.java.historicaldatafetcher;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.TimeZone;
+
+import com.ib.client.Contract;
+import com.ib.client.Types;
+import com.ib.controller.ApiConnection;
+import com.ib.controller.ApiController;
+
+import samples.testbed.contracts.ContractSamples;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		ApiController apiController = new ApiController(new ConnectionHandler(), new InLogger(), new OutLogger());
+		apiController.connect("127.0.0.1", 4002, 3, "");
+
+		Contract contract = ContractSamples.SPXIndex();
+		Calendar from = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+		from.set(2022, Calendar.JANUARY, 0, 0, 0, 0);
+		Calendar to = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+		to.set(2023, 0, 0, 0, 0, 0);
+
+		HistoricalDataHandler historicalDataHandler = new HistoricalDataHandler(
+				apiController,
+				Types.BarSize._1_day,
+				Types.WhatToShow.TRADES, from, to,
+				contract,
+				true);
+		historicalDataHandler.fetchCandlesticks();
+	}
+}
+
+class InLogger implements ApiConnection.ILogger {
+	@Override
+	public void log(String valueOf) {
+		System.out.println("In message: " + valueOf);
+	}
+}
+
+class OutLogger implements ApiConnection.ILogger {
+	@Override
+	public void log(String valueOf) {
+		System.out.println("Out message: " + valueOf);
+	}
+}
+
+class ConnectionHandler implements ApiController.IConnectionHandler {
+
+	@Override
+	public void connected() {
+
+	}
+
+	@Override
+	public void disconnected() {
+
+	}
+
+	@Override
+	public void accountList(List<String> list) {
+
+	}
+
+	@Override
+	public void error(Exception e) {
+		System.out.println("Error occurred: " + e.getMessage());
+	}
+
+	@Override
+	public void message(int id, int errorCode, String errorMsg, String advancedOrderRejectJson) {
+		System.out.println("Got message: " + errorMsg);
+	}
+
+	@Override
+	public void show(String string) {
+		System.out.println("Showing:" + string);
+	}
+}
